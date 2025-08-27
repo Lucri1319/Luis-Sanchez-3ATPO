653bd270783913ba55c2045daceb5d6ddbfb9c1e
Luis Sanchez <66450603+Lucri1319@users.noreply.github.com>
2025-08-27 11:02:19 -0600

    Add files via upload

---
+++ esquividi_tercero_a_tepeo/metodos_de_cadena.py
@@ -0,0 +1,10 @@
+cadena1 = "luis"
+print("capitalize", cadena1.capitalize())
+cadena2 = "ESQUividi"
+print("casefold", cadena2.casefold())
+print(cadena2.center(10, '🐒'))
+menus = ["Menu atm", "Depositar", "Retirar", "Consultar"]
+for menu in menus :
+    print(menu.center(20),'-')
+cadena3 = "Hola Mundo"
+print (cadena3.count("o",3,10)) 
